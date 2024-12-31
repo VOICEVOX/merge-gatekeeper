0ff53764f91c2db2d58786302e32d3f67c75cbc2
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-12-31 14:10:59 +0900

    Add: ビルドチェックを追加

--- build.ts
+++ build.ts
@@ -1,11 +1,34 @@
 import * as rolldown from "rolldown";
+import fs from "node:fs/promises"
 
-await rolldown.build({
+const output = await rolldown.build({
   input: "src/index.ts",
   platform: "node",
   output: {
-    dir: "dist",
     minify: true,
   },
   treeshake: true,
 });
+
+const isCheck = process.argv.includes("--check");
+
+const mainFile = output.output.find((file) => file.fileName === "index.js");
+if (!mainFile) {
+  throw new Error("index.js not found");
+}
+if (mainFile.type !== "chunk") {
+  throw new Error("index.js is not a chunk");
+}
+
+const outputPath = `${import.meta.dirname}/dist/index.js`;
+if (isCheck) {
+  const expected = await fs.readFile(outputPath, "utf-8");
+  if (mainFile.code !== expected) {
+    console.error("The output is not up-to-date");
+    process.exit(1);
+  }
+  console.log("The output is up-to-date");
+} else {
+  await fs.writeFile(outputPath, mainFile.code);
+  console.log("Build completed");
+}
