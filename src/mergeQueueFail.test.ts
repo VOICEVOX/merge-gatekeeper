d4980e9d510a7ad1bc85fd364dc241493e5519a5
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-01-04 09:43:56 +0900

    Code: フォーマットをかける

--- src/mergeQueueFail.test.ts
+++ src/mergeQueueFail.test.ts
@@ -1,9 +1,12 @@
-import test from "node:test";
 import * as assert from "node:assert";
 import child_process from "node:child_process";
+import test from "node:test";
 
 test("マージキューだけ落ちるテスト", async () => {
-  const stdout = child_process.execSync("git branch --show-current").toString().trim();
+  const stdout = child_process
+    .execSync("git branch --show-current")
+    .toString()
+    .trim();
 
   assert.ok(!stdout.includes("gh-readonly-queue"));
-})
+});
